eb0d005b1a9be50d4e9ff3e1a31b9b145fef5fb8
ykarpa <yurii.karpa@lnu.edu.ua>
2023-10-06 15:12:28 +0300

    Add project files.

---
+++ Lab04/MainForm.cs
@@ -0,0 +1,15 @@
+namespace Lab04
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
